9250ae844f3cdd3631791b04a22165977fbfe755
amccadams <amccadams@gmail.com>
2013-05-31 14:43:42 -0500

    rails generator

--- test/integration/anonymous_post_article_test.rb
+++ test/integration/anonymous_post_article_test.rb
@@ -5,11 +5,11 @@ class AnonymousPostArticleTest < ActionDispatch::IntegrationTest
     Post.create!(title: "First Post!!", body: "This is awkward...I have nothing to say :(")
     Post.create!(title: "Wow, it's been a while!", body: "And yet, I;m still not a writter :( :(")
     
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
+    visit '/posts'
+    # click_link "View All Posts"
+    # assert_current_path '/posts'
     #assume we have a page variable
-    assert_include page.content, "First Post!!"
-    assert_include page.content, "Wow, it's been a while!"
+    assert_include page.body, "First Post!!"
+    assert_include page.body, "Wow, it's been a while!"
   end
 end
