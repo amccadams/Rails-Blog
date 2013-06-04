5d5c8e90ab996a6e047b7ad53bef16075a0793f6
amccadams <amccadams@gmail.com>
2013-06-04 13:06:23 -0500

    article create

--- test/integration/anonymous_post_article_test.rb
+++ test/integration/anonymous_post_article_test.rb
@@ -1,15 +1,14 @@
 require 'test_helper'
 
-class AnonymousPostArticleTest < ActionDispatch::IntegrationTest
-  test "browse articles" do
-    Post.create!(title: "First Post!!", body: "This is awkward...I have nothing to say :(")
-    Post.create!(title: "Wow, it's been a while!", body: "And yet, I;m still not a writter :( :(")
-    
+class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
+  test "posting a new article" do
+    visit '/'
+    click_link 'Write Post'
+    fill_in 'Title', with: 'My First Post!!'
+    fill_in 'Body', with: "I don't have anything to say"
+    click_button 'Submit Post'
+    assert_include page.body, "Your post has now been published!"
     visit '/posts'
-    # click_link "View All Posts"
-    # assert_current_path '/posts'
-    #assume we have a page variable
-    assert_include page.body, "First Post!!"
-    assert_include page.body, "Wow, it's been a while!"
+    assert_include page.body, 'My First Post!!'
   end
 end
